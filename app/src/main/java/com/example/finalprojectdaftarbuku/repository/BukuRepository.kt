a815ea112b19ec65dcfd7d5f5dd6e4259fa4ecda
RezaAzharii <rezaazhari867@gmail.com>
2025-01-28 17:24:17 +0700

    Menambahkan NetworkBukuRepository dengan fungsi CRUD untuk data Buku

--- app/src/main/java/com/example/finalprojectdaftarbuku/repository/BukuRepository.kt
+++ app/src/main/java/com/example/finalprojectdaftarbuku/repository/BukuRepository.kt
@@ -2,6 +2,8 @@ package com.example.finalprojectdaftarbuku.repository
 
 import com.example.finalprojectdaftarbuku.model.AllBukuResponse
 import com.example.finalprojectdaftarbuku.model.Buku
+import com.example.finalprojectdaftarbuku.serviceapi.BukuService
+import okio.IOException
 
 interface BukuRepository{
     suspend fun getBuku(): AllBukuResponse
@@ -11,4 +13,44 @@ interface BukuRepository{
     suspend fun insertBuku(buku: Buku)
     suspend fun updateBuku(idBuku: Int, buku: Buku)
     suspend fun deleteBuku(idBuku: Int)
+}
+
+class NetworkBukuRepository(
+    private val bukuApiService: BukuService
+): BukuRepository{
+    override suspend fun getBuku(): AllBukuResponse =
+        bukuApiService.getAllBuku()
+
+    override suspend fun getSPBuku(): AllBukuResponse =
+        bukuApiService.getSPBuku()
+
+    override suspend fun getSHBuku(): AllBukuResponse =
+        bukuApiService.getSHBuku()
+
+    override suspend fun getBukubyID(idBuku: Int): Buku {
+        return bukuApiService.getBukubyID(idBuku).data
+    }
+
+    override suspend fun insertBuku(buku: Buku) {
+        bukuApiService.insertBuku(buku)
+    }
+
+    override suspend fun updateBuku(idBuku: Int, buku: Buku) {
+        bukuApiService.updateBuku(idBuku, buku)
+    }
+
+    override suspend fun deleteBuku(idBuku: Int) {
+        try {
+            val response = bukuApiService.deleteBuku(idBuku)
+            if (!response.isSuccessful){
+                throw IOException("Gagal menghapus Buku. Http Status code: " +
+                        "${response.code()}")
+            }else{
+                response.message()
+                println(response.message())
+            }
+        }catch (e: Exception){
+            throw e
+        }
+    }
 }
