c83bb1828d70b13b482d48bf8d3205bbeb949257
RezaAzharii <rezaazhari867@gmail.com>
2025-01-28 17:28:18 +0700

    Menambahkan NetworkKategoriRepository dengan fungsi CRUD untuk data Kategori

--- app/src/main/java/com/example/finalprojectdaftarbuku/repository/KategoriRepository.kt
+++ app/src/main/java/com/example/finalprojectdaftarbuku/repository/KategoriRepository.kt
@@ -2,6 +2,8 @@ package com.example.finalprojectdaftarbuku.repository
 
 import com.example.finalprojectdaftarbuku.model.ALlKategoriResponse
 import com.example.finalprojectdaftarbuku.model.Kategori
+import com.example.finalprojectdaftarbuku.serviceapi.KategoriService
+import java.io.IOException
 
 interface KategoriRepository {
     suspend fun getKategori(): ALlKategoriResponse
@@ -9,4 +11,39 @@ interface KategoriRepository {
     suspend fun insertKategori(kategori: Kategori)
     suspend fun updateKategori(idKategori: Int, kategori: Kategori)
     suspend fun deleteKategori(idKategori: Int)
+}
+
+data class NetworkKategoriRepository(
+    private val kategoriService: KategoriService
+): KategoriRepository{
+    override suspend fun getKategori(): ALlKategoriResponse =
+        kategoriService.getAllKategori()
+
+    override suspend fun getKategoriById(idKategori: Int): Kategori{
+        return kategoriService.getKategoribyID(idKategori).data
+    }
+
+    override suspend fun insertKategori(kategori: Kategori) {
+        kategoriService.insertKategori(kategori)
+    }
+
+    override suspend fun updateKategori(idKategori: Int, kategori: Kategori) {
+        kategoriService.updateKategori(idKategori, kategori)
+    }
+
+    override suspend fun deleteKategori(idKategori: Int) {
+        try {
+            val response = kategoriService.deleteKategori(idKategori)
+            if (!response.isSuccessful){
+                throw IOException("Gagal menghapus Buku. Http Status code: " +
+                        "${response.code()}")
+            }else{
+                response.message()
+                println(response.message())
+            }
+        }catch (e: Exception){
+            throw e
+        }
+    }
+
 }
