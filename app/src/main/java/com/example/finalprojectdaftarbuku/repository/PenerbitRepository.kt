0529535b65c7ac8afdebfeac811605ce2c67a0b2
RezaAzharii <rezaazhari867@gmail.com>
2025-01-28 17:30:46 +0700

    Menambahkan NetworkPenerbitRepository untuk menangani operasi CRUD pada data Penerbit melalui PenerbitService

--- app/src/main/java/com/example/finalprojectdaftarbuku/repository/PenerbitRepository.kt
+++ app/src/main/java/com/example/finalprojectdaftarbuku/repository/PenerbitRepository.kt
@@ -2,6 +2,8 @@ package com.example.finalprojectdaftarbuku.repository
 
 import com.example.finalprojectdaftarbuku.model.AllPenerbitResponse
 import com.example.finalprojectdaftarbuku.model.Penerbit
+import com.example.finalprojectdaftarbuku.serviceapi.PenerbitService
+import okio.IOException
 
 interface PenerbitRepository{
     suspend fun getPenerbit(): AllPenerbitResponse
@@ -9,4 +11,39 @@ interface PenerbitRepository{
     suspend fun insertPenerbit(penerbit: Penerbit)
     suspend fun updatePenerbit(idPenerbit: Int, penerbit: Penerbit)
     suspend fun deletePenerbit(idPenerbit: Int)
+}
+
+data class NetworkPenerbitRepository(
+    private val penerbitService: PenerbitService
+): PenerbitRepository{
+    override suspend fun getPenerbit(): AllPenerbitResponse =
+        penerbitService.getAllPenerbit()
+
+    override suspend fun getPenerbitById(idPenerbit: Int): Penerbit {
+        return penerbitService.getPenerbitbyID(idPenerbit).data
+    }
+
+    override suspend fun insertPenerbit(penerbit: Penerbit) {
+        penerbitService.insertPenerbit(penerbit)
+    }
+
+    override suspend fun updatePenerbit(idPenerbit: Int, penerbit: Penerbit) {
+        penerbitService.updatePenerbit(idPenerbit, penerbit)
+    }
+
+    override suspend fun deletePenerbit(idPenerbit: Int) {
+        try {
+            val response = penerbitService.deletePenerbit(idPenerbit)
+            if (!response.isSuccessful){
+                throw IOException("Gagal menghapus Buku. Http Status code: " +
+                        "${response.code()}")
+            }else{
+                response.message()
+                println(response.message())
+            }
+        }catch (e: Exception){
+            throw e
+        }
+    }
+
 }
